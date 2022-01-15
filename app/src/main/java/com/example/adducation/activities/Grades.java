47390301289c51d63168db67403b4fb8b232ee23
Justin Tung <tungjust@gmail.com>
2022-01-15 15:00:06 -0800

    Pages added w scrollview

---
+++ app/src/main/java/com/example/adducation/activities/Grades.java
@@ -0,0 +1,14 @@
+package com.example.adducation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Grades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grades);
+    }
+}
